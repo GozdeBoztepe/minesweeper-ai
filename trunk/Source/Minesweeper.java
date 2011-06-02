ed23d7f29b75dcd46c86dc5cd37917d354f7f0fb
shri90 <shri90@86380405-5eea-1429-da4b-a2950368a37f>
2011-06-02 19:58:51 +0000

    GUI base work in progress

---
+++ trunk/Source/Minesweeper.java
@@ -0,0 +1,31 @@
+// Lib.java -- ICS 52 -- Lib system
+
+class Minesweeper
+{
+	
+	public static void main(String[] commandLineArguments)
+	{
+		String asciiFileName = null;
+		if (commandLineArguments.length > 0)
+			asciiFileName = commandLineArguments[0];
+		
+		Minesweeper minesweeper = new Minesweeper(asciiFileName);
+		
+		MineFrame mf = new MineFrame(minesweeper);
+	}
+
+
+	public Minesweeper(String fileToLoad)
+	{
+		
+		
+		
+	}
+
+	public void exitSystem()
+	{
+		// A good place to do clean-up, such as writing to files.
+		System.exit(0);
+	}
+	
+}
